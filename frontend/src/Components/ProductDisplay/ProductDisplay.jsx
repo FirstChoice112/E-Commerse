a2d0a7839f13710ea065b5e12f7af3979342e466
FirstChoice112 <johan.skoog.1986@gmail.com>
2024-10-06 12:31:36 +0200

    Added responsivness to the breadcrum, cartitems, descriptionbox, productdisplay and relatedProducts in 1280, 1024, 800 and 500. Front-end part stage 1 is now complete

--- frontend/src/Components/ProductDisplay/ProductDisplay.jsx
+++ frontend/src/Components/ProductDisplay/ProductDisplay.jsx
@@ -41,6 +41,7 @@ const ProductDisplay = (props) => {
         </div>
         <div className="productdisplay-right-description">
           A lightweight, usually knitted, pullover shirt, close-fitting and
+          short sleeves, worn as an undershirt or outer garment..
         </div>
         <div className="productdisplay-right-size">
           <h1>Select Size</h1>
